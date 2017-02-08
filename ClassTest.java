8efe2c9aa27b4b707284b092fbfc4278197e820c
Paul Du <du.paul136@gmail.com>
2017-02-08 17:39:06 +0100

    Correction typo

--- ClassTest.java
+++ ClassTest.java
@@ -2,6 +2,6 @@ public class ClassTest{
     
     public static void main(String[] args){
         System.out.println("c est un succes !!");
-        System.out.println("c est a nouveau un succes !!");
+        System.out.println("C'est a nouveau un succès !!!");
     }
 }
